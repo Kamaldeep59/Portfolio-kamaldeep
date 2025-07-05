1d8dc96e0626a19700d7ddf65c4b9f38caa66319
Kamaldeep59 <105438212+Kamaldeep59@users.noreply.github.com>
2025-07-05 16:18:33 +0530

    done

--- src/User.tsx
+++ src/User.tsx
@@ -1,8 +1,8 @@
 import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
 const Info = {
-    name: "Kamaldeep",
-    stack: ["Software Developer", "Full Stack Developer"],
-    bio: "I'm a passionate software developer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
+    name: "Kamaldeep Sokhal",
+    stack: ["Front-End Developer","Software Developer", "Java Developer"],
+    bio: "Enthusiastic and detail-oriented frontend developer with 6 months of internship experience in React.js, focused on building responsive and user-centric web applications. Seeking an entry-level role to apply my skills in JavaScript, React, and modern UI design to create impactful digital solutions."
 }
 
 
@@ -10,66 +10,49 @@ const Info = {
 const ProjectInfo = [
 
     {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
+        title: "Electronic Store",
+        desc: "Electronic Store is a modern, fully responsive e-commerce platform built using React, TailwindCSS, and HeadlessUI, offering a seamless shopping experience across all Electronic devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Electronic Store combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
+        image: "store.png",
         live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
+        technologies: ["React", "Tailwind", "Redux", "SpringBoot","MYSQL"],
+        link: "",
+        github: ""
     },
+    {
+        title: "Smart Contact Manager(SCM)",
+        desc: "Developed a responsive React-based UI for contact management.Implemented login/signup forms, contact dashboard, and CRUD interfaces.Used Axios to connect with backend APIs and manage user authentication flow.",
+        image: "scm.png",
+        live: false,
+        technologies: ["React", "Springboot", "MYSQL", "Tailwind"],
+        link: "",
+        github: ""
+    },
+    {
+        title: "E-Comm",
+        desc: "E-Comm is a full-stack web application that replicates the core features of ecom,Built the user interface for an electronic products store with filtering and sorting features.Integrated product listings, categories, and cart UI using functional components.Implemented routing, dynamic UI updates, and state management using hooks.",
+        image: "Ekart.png",
+        live: false,
+        technologies: ["React", "Spring?Boot", "Tailwind",  "MYSQL",],
+        link: "",
+        github: ""
+     },
     // {
-    //     title: "Facebook Clone",
-    //     desc: "Facebook Clone is a full-stack web application that replicates the core features of Facebook, including user authentication, posting, and commenting. The front end is built with React, TailwinCSS, Tabler-Icons, Mantine and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, post updates, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Facebook Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-    //     image: "Facebook.png",
-    //     live: false,
-    //     technologies: ["React", "Springboot", "MongoDB", "Tailwind", "Tabler-Icons", "Firebase"],
-    //     link: "https://github.com/Code-Mars/Facebook-Clone",
-    //     github: "https://github.com/Code-Mars/Facebook-Clone"
-    // },
-    // {
-    //     title: "Spotify Clone",
-    //     desc: "Spotify Clone is a full-stack web application that replicates the core features of Spotify, including user authentication, music playback, and playlist creation. The front end is built with React, TailwindCSS, Heroicons, and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, search for songs, and create playlists. The app features real-time updates, so users can see new songs as they are added. Spotify Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-    //     image: "Spotify.png",
-    //     live: false,
-    //     technologies: ["React", "Springboot", "Tailwind", "Tabler-Icons", "MongoDB", "Firebase"],
-    //     link: "https://github.com/Code-Mars/Spotify-Clone",
-    //     github: "https://github.com/Code-Mars/Spotify-Clone"
-    // },
-    // {
-    //     title: "Travel Tracker",
+    //     title: "Blog-App.",
     //     desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
     //     image: "Travel.png",
     //     live: false,
-    //     technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
-    //     link: "https://github.com/Code-Mars/Travel-Tracker",
-    //     github: "https://github.com/Code-Mars/Travel-Tracker"
+    //     technologies: ["React", "SpringBoot", "MYSQL", ],
+    //     link: "",
+    //     github: ""
     // },
-    // {
-    //     title: "Instagram Clone",
-    //     desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-    //     image: "Instagram.png",
-    //     live: false,
-    //     technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
-    //     link: "https://github.com/Code-Mars/Instagram-Clone",
-    //     github: "https://github.com/Code-Mars/Instagram-Clone"
-    // },
-    // {
-    //     title: "CodeX Code Editor",
-    //     desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
-    //     image: "CodeX.png",
-    //     live: false,
-    //     technologies: ["React", "Tailwind", "Ace Editor"],
-    //     link: "https://github.com/Code-Mars/CodeX",
-    //     github: "https://github.com/Code-Mars/CodeX"
-    // }
+    
 ]
 
 
 const SkillInfo = [
     {
         title: "Frontend",
-        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Tailwind CSS", "Material UI", "Bootstrap"
+        skills: ["HTML", "CSS","JavaScript", "React JS", "Tailwind CSS", "Material UI", "Bootstrap"
         ]
     },
     {
@@ -96,30 +79,27 @@ const socialLinks = [
 
 const ExperienceInfo = [
     {
-        role: "Software developer",
-        company: "Natural",
-        date: "Oct 2023 - december 2023",
-        desc: "I led software development with Spring Boot, React, and Angular, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
-        skills: ["Springboot", "React JS", "Node JS", "MySQL", "MongoDB", "Microservices"]
-    },
-    {
-        role: "System Engineer",
-        company: "Infosys",
-        date: "Nov 2022 - Sep 2023",
-        desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
-        skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
+        role: "Software Programmer",
+        company: "Natural Group(NSCSPL)",
+        date: "July 2023 - December 2023",
+        desc: "I led software development with Spring Boot, React, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
+        skills: ["React JS","SpringBoot","Hibernate","JPA", "MYSQL","QA", "Microservices"]
     }
+    // ,
+    // {
+    //     role: "System Engineer",
+    //     company: "Infosys",
+    //     date: "Nov 2022 - Sep 2023",
+    //     desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
+    //     skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
+    // }
 ]
 const Slugs = [
     "typescript",
     "spring",
     "javascript",
-    "dart",
     "java",
     "react",
-    "angular",
-    "flutter",
-    "android",
     "html5",
     "css3",
     "springboot",
@@ -127,25 +107,18 @@ const Slugs = [
     "selenium",
     "nodedotjs",
     "express",
-    "nextdotjs",
-    "prisma",
     "mysql",
     "amazonaws",
     "postgresql",
     "firebase",
     "nginx",
-    "vercel",
     "testinglibrary",
-    "jest",
-    "cypress",
     "docker",
     "git",
     "jira",
     "github",
     "gitlab",
     "visualstudiocode",
-    "androidstudio",
-    "sonarqube",
-    "figma",
+    "IntelliJ IDEA"
 ];
 export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
